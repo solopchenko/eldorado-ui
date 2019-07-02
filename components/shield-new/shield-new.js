2fc435c9f0d6b3fdf3cddd9d83f31276fc3e8535
Svyatoslav Solopchenko <svyatoslav.solopchenko@eldorado.ru>
2019-07-02 10:32:44 +0300

    Добавлен компонент новых шильдиков

---
+++ components/shield-new/shield-new.js
@@ -0,0 +1,30 @@
+class ShieldNew {
+  constructor(shield) {
+    this.shield = shield;
+    let shield__content = this.shield.querySelector('.shield-new__content');
+
+    this.toggle = () => {
+      this.shield.classList.toggle('shield-new--show');
+    }
+
+    this.show = () => {
+      this.shield.classList.add('shield-new--show');
+    }
+
+    this.hide = () => {
+      this.shield.classList.remove('shield-new--show');
+    }
+
+    this.shield.addEventListener('mouseover', this.show);
+    this.shield.addEventListener('mouseout', this.hide);
+  }
+
+  static createShields() {
+    let elements = document.querySelectorAll('.shield-new');
+    return Array.from(elements).map((element) => {
+      return new ShieldNew(element);
+    });
+  }
+}
+
+window.shields = ShieldNew.createShields();
